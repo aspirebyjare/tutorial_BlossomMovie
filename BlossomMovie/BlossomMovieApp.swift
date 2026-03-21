e65f98e4a02a9fdf0d754de5b816a62f6941d215
Jare <aspirebyjare@gmail.com>
2026-03-21 12:57:22 -0700

    initial commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Jared Smith on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
